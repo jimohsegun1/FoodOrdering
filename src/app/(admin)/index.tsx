fcdec556e9ad8904f4853eaf6d50673c2f860a96
jimohsegun1 <jimohsegunj@gmail.com>
2025-02-20 14:07:53 +0100

    top tab navigation

--- src/app/(admin)/index.tsx
+++ src/app/(admin)/index.tsx
@@ -1,5 +1,15 @@
-import { Redirect } from 'expo-router';
+import { FlatList } from 'react-native';
+import products from '@assets/data/products';
+import ProductListItem from '@components/ProductListItem';
 
-export default function TabIndex() {
-  return <Redirect href={'/(admin)/menu/'} />;
+export default function MenuScreen() {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
+  );
 }
--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -1,47 +1,29 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import Colors from '@/constants/Colors';
+import { FontAwesome } from '@expo/vector-icons';
+import { Link, Stack } from 'expo-router';
+import { Pressable } from 'react-native';
 
-import Colors from '../../constants/Colors';
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={20} style={{ marginBottom: -3 }} {...props} />;
-}
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function MenuStack() {
   return (
-    <Tabs
+    <Stack
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="shopping-cart"
+                  size={25}
+                  color={Colors.light.tint}
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
       }}
     >
-      <Tabs.Screen name="index" options={{ href: null }} />
-
-      <Tabs.Screen
-        name="menu"
-        options={{
-          title: 'Menu',
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="cutlery" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Orders',
-          tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
-        }}
-      />
-    </Tabs>
+      <Stack.Screen name="index" options={{ title: 'Menu' }} />
+    </Stack>
   );
 }
