309f6fac49c91f5293b654159855d431399c8e8e
jimohsegun1 <jimohsegunj@gmail.com>
2025-02-26 16:30:13 +0100

    authentication

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
-import 'react-native-url-polyfill/auto';
-import * as SecureStore from 'expo-secure-store';
-import { createClient } from '@supabase/supabase-js';
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
@@ -14,9 +14,8 @@ const ExpoSecureStoreAdapter = {
   },
 };
 
-const supabaseUrl = 'https://lxkvgdxeyinsgipovfeh.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4a3ZnZHhleWluc2dpcG92ZmVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ3ODk4NDEsImV4cCI6MjAxMDM2NTg0MX0.aSf3X0QjalHWUc3DeSWBvAsrEpQu3b9gB3O9BGk-CRk';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
